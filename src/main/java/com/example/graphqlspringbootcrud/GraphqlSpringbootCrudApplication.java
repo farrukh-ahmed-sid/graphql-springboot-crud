a43530f4f966b88be26e7314abc755269c2a864b
farrukh-ahmed-sid <mr.farrukh.ahmed@gmail.com>
2022-06-26 00:44:12 +0500

    first commit

---
+++ src/main/java/com/example/graphqlspringbootcrud/GraphqlSpringbootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlspringbootcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringbootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringbootCrudApplication.class, args);
+	}
+
+}
